edd618fa91d483dec082d43d2dbd93deb8a9a145
fuddms2 <fuddms2@gmail.com>
2023-05-06 21:02:39 +0900

    test 4

--- src/main/java/com/teamproject/CorsConfig.java
+++ src/main/java/com/teamproject/CorsConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
     
-    //* test 입니다 이번만은 제발제발 됐으면! */
+    //* test 입니다 이번만은 제발제발 됐으면! 왜안되는거지 */
 
     public void addCorsMappings(CorsRegistry registry) {
         registry

a9d568c530d1f197f27817b749a3e1da44db486a
fuddms2 <fuddms2@gmail.com>
2023-05-06 20:42:07 +0900

    test retry2

---
+++ src/main/java/com/teamproject/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.teamproject.devTalks.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    
+    //* test 입니다 이번만은 제발제발 됐으면! */
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*");
+    }
+
+}

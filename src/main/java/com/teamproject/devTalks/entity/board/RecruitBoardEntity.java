286e92f1719b7190695b69a8278fc86d558d5853
JinDoL1998 <jkhgfdds1@gmail.com>
2023-05-09 13:03:51 +0900

    recruit entity

--- src/main/java/com/teamproject/devTalks/entity/board/RecruitBoardEntity.java
+++ src/main/java/com/teamproject/devTalks/entity/board/RecruitBoardEntity.java
@@ -1,5 +1,34 @@
 package com.teamproject.devTalks.entity.board;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="recruit")
+@Table(name="recruit")
 public class RecruitBoardEntity {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int recruitBoardNumber;
+    private String writerProfileImageUrl;
+    private String writerEmail;
+    private String writerNickname;
+    private String writeDatetime;
+    private String updateDatetime;
+    private String recruitTitle;
+    private String recruitContent;
+    private String recruitImageUrl;
+    private String recruitmentStatus;
+    private int viewCount;
+
 }

2b5960bf8b5448e277ebeed27fc0d89d9f28666d
SSoyoo <116279350+SSoyoo@users.noreply.github.com>
2023-05-09 17:28:47 +0900

    Revert "폴더위치수정, service어노테이션 작성"

--- src/main/java/com/teamproject/devTalks/service/implement/user/AdminServiceImplement.java
+++ src/main/java/com/teamproject/devTalks/service/implement/user/AdminServiceImplement.java
@@ -1,9 +1,5 @@
 package com.teamproject.devTalks.service.implement.user;
 
-import com.teamproject.devTalks.service.user.AdminService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class AdminServiceImplement implements AdminService {
+public class AdminServiceImplement {
     
 }

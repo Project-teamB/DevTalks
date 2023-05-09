52d209487a1805b9651123aa4406229344f1327b
ssoyoo <soyoo0819@naver.com>
2023-05-09 12:58:34 +0900

    유저해시태그 저장 로직 작성

--- src/main/java/com/teamproject/devTalks/service/implement/user/UserServiceImplement.java
+++ src/main/java/com/teamproject/devTalks/service/implement/user/UserServiceImplement.java
@@ -3,7 +3,9 @@ package com.teamproject.devTalks.service.implement.user;
 import com.teamproject.devTalks.common.util.CustomResponse;
 import com.teamproject.devTalks.dto.request.user.PostUserRequestDto;
 import com.teamproject.devTalks.dto.response.ResponseDto;
+import com.teamproject.devTalks.entity.hashTag.UserHashTagEntity;
 import com.teamproject.devTalks.entity.user.UserEntity;
+import com.teamproject.devTalks.repository.hashTag.UserHashTagRepository;
 import com.teamproject.devTalks.repository.user.UserRepository;
 import com.teamproject.devTalks.service.user.UserService;
 import lombok.RequiredArgsConstructor;
@@ -11,11 +13,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UserServiceImplement implements UserService {
 
     private final UserRepository userRepository;
+    private final UserHashTagRepository userHashTagRepository;
     private final BCryptPasswordEncoder passwordEncoder;
 
 
@@ -25,6 +31,8 @@ public class UserServiceImplement implements UserService {
         String userEmail = dto.getUserEmail();
         String userNickname = dto.getUserNickname();
         String userPhoneNumber = dto.getUserPhoneNumber();
+
+
 
         String encodedUserPassword = passwordEncoder.encode(dto.getUserPassword());
         dto.setUserPassword(encodedUserPassword);
@@ -42,6 +50,32 @@ public class UserServiceImplement implements UserService {
 
             UserEntity userEntity = new UserEntity(dto);
             userRepository.save(userEntity);
+
+            int userNumber = userEntity.getUserNumber();
+            List<String> userHashTagList = dto.getUserHashtag();
+            List<UserHashTagEntity> hashTagList = new ArrayList<>();
+
+
+            for(String hashTag : userHashTagList) {
+
+                UserHashTagEntity userHashTagEntity = new UserHashTagEntity();
+
+                userHashTagEntity.setUserNumber(userNumber);
+                userHashTagEntity.setHashTag(hashTag);
+
+                hashTagList.add(userHashTagEntity);
+
+            }
+
+
+
+
+
+
+
+
+
+
 
         }catch (Exception exception){
             exception.printStackTrace();

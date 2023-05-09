27c9734102a90d0afe7aeeedb99f4aa75338eaef
ghyun100 <bgh3508@naver.com>
2023-05-09 10:02:11 +0900

    qnaEntity작성

--- src/main/java/com/teamproject/devTalks/entity/comment/QnaCommentEntity.java
+++ src/main/java/com/teamproject/devTalks/entity/comment/QnaCommentEntity.java
@@ -1,5 +1,29 @@
 package com.teamproject.devTalks.entity.comment;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Entity(name = "qnaComment")
+@Table(name = "qnaComment")
 public class QnaCommentEntity {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int qnaCommentNumber;
+    private String commentContent;
+    private String writerProfileImageUrl;
+    private String writerNickname;
+    private String writerEmail;
+    private String writeDatetime;
+    private String userNumber;
+    private String quaBoardNumber;
+
 }
--- src/main/java/com/teamproject/devTalks/entity/hashTag/QnaBoardHashTagEntity.java
+++ src/main/java/com/teamproject/devTalks/entity/hashTag/QnaBoardHashTagEntity.java
@@ -1,5 +1,21 @@
 package com.teamproject.devTalks.entity.hashTag;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Entity(name = "qnaHashtag")
+@Table(name = "qnaHashtag")
 public class QnaBoardHashTagEntity {
     
+    @Id
+    private String hashtag;
+    @Id
+    private int qnaBoardNumber;
+
 }

dc39e95a5ce84f0ce6e272faea6b26514e4dcdfe
fuddms2 <fuddms2@gmail.com>
2023-05-10 15:18:39 +0900

    InformationBoardEntity 작성

--- src/main/java/com/teamproject/devTalks/dto/request/board/postBoard/PostInformationBoardRequestDto.java
+++ src/main/java/com/teamproject/devTalks/dto/request/board/postBoard/PostInformationBoardRequestDto.java
@@ -1,5 +1,24 @@
 package com.teamproject.devTalks.dto.request.board.postBoard;
 
+import javax.validation.constraints.NotBlank;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class PostInformationBoardRequestDto {
-    
+    @NotBlank
+    private String informationBoardTitle;
+    @NotBlank
+    private String informationBoardContent;
+    private String informationBoardImageUrl;
+    private String contentSource;
+
+    public PostInformationBoardRequestDto(PostInformationBoardRequestDto dto) {
+        this.informationBoardTitle = dto.getInformationBoardTitle();
+        this.informationBoardContent = dto.getInformationBoardContent();
+        this.informationBoardImageUrl = dto.getInformationBoardImageUrl();
+        this.contentSource = dto.getContentSource();
+    }
 }

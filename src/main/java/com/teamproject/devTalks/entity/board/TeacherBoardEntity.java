6c5e6c173840619a4b85d6a8c5b1b1e57f8e90f1
hankk <joseph10987@naver.com>
2023-05-10 13:39:44 +0900

    2023.05.10 teacher

--- src/main/java/com/teamproject/devTalks/entity/board/TeacherBoardEntity.java
+++ src/main/java/com/teamproject/devTalks/entity/board/TeacherBoardEntity.java
@@ -1,5 +1,31 @@
 package com.teamproject.devTalks.entity.board;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity(name = "teacher")
+@Table(name = "teacher")
 public class TeacherBoardEntity {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int teacherBoardNumber;
+    private String writerProfileImageUrl;
+    private String writerNickname;
+    private String writeDatetime;
+    private String teacherTitle;
+    private String teacherContent;
+    private String teacherBoardImageUrl;
+    private String career;
+    private String lectureUrl;
+    private int viewCount;
+    private String category;
 }

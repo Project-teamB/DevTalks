6fff42e8a81bbeef81dc776b60ad08d49cb02423
JinDoL1998 <jkhgfdds1@gmail.com>
2023-05-10 16:26:23 +0900

    recruit dto, repository

--- src/main/java/com/teamproject/devTalks/dto/request/board/patchBoard/PatchRecruitBoardRequestDto.java
+++ src/main/java/com/teamproject/devTalks/dto/request/board/patchBoard/PatchRecruitBoardRequestDto.java
@@ -1,5 +1,26 @@
 package com.teamproject.devTalks.dto.request.board.patchBoard;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+
 public class PatchRecruitBoardRequestDto {
     
+    @NotBlank
+    @Email
+    private String writerEmail;
+    @NotBlank
+    private int recruitBoardNumber;
+    @NotBlank
+    private String recruitTitle;
+    @NotBlank
+    private String recruitContent;
+    private String recruitImageUrl;
+    private boolean recruitmentStatus;
+
 }

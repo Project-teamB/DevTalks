fec6b7c04d626ea0761b99962bda86b4e5effd0a
fuddms2 <fuddms2@gmail.com>
2023-05-10 17:46:49 +0900

    informationEntity작성, imformation pk class 생성

--- src/main/java/com/teamproject/devTalks/entity/heart/InformationHeartEntity.java
+++ src/main/java/com/teamproject/devTalks/entity/heart/InformationHeartEntity.java
@@ -1,5 +1,25 @@
 package com.teamproject.devTalks.entity.heart;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+
+import com.teamproject.devTalks.entity.primaryKey.information.InformationHeartPk;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="InformationHeart")
+@Table(name="InformationHeart")
+@IdClass(InformationHeartPk.class)
 public class InformationHeartEntity {
-    
+    @Id
+    private int informationBoardNumber;
+    @Id
+    private int userNumber;
 }

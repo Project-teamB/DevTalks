e737ae90750a55331b35d5f1639e968bcff1584b
MINJU0401 <126844522+MINJU0401@users.noreply.github.com>
2023-05-11 10:40:15 +0900

    boardListResultSet 리팩터링 외 listResponseDto 작성중

---
+++ src/main/java/com/teamproject/devTalks/entity/resultSet/InformationBoardListResultSet.java
@@ -0,0 +1,15 @@
+package com.teamproject.devTalks.entity.resultSet;
+
+public class InformationBoardListResultSet {
+    private int getInformationBoardNumber;
+    private String getWriterProfileImageUrl;
+    private String getWriterNickname;
+    private String getWriterEmail;
+    private String getInformationBoardTitle;
+    private String getInformationBoardContent;
+    private String getInformationBoardImageUrl;
+    private String getContentSource;
+    private int getViewCount;
+    private String getWriteDatetime;
+    private String getUpdateDatetime;
+}

59768a7ababfc8b7a70f4dd5635cccd8fa8534b8
MINJU0401 <126844522+MINJU0401@users.noreply.github.com>
2023-05-11 12:19:00 +0900

    InformationBoardService 작성

--- src/main/java/com/teamproject/devTalks/service/board/InformationBoardService.java
+++ src/main/java/com/teamproject/devTalks/service/board/InformationBoardService.java
@@ -1,5 +1,22 @@
 package com.teamproject.devTalks.service.board;
 
-public class InformationBoardService {
+import org.springframework.http.ResponseEntity;
+
+import com.teamproject.devTalks.dto.response.board.GetInformationBoardResponseDto;
+import com.teamproject.devTalks.dto.response.board.GetInformationBoardListResponseDto;
+import com.teamproject.devTalks.dto.request.board.patchBoard.PatchInformationBoardRequestDto;
+import com.teamproject.devTalks.dto.request.board.postBoard.PostInformationBoardRequestDto;
+import com.teamproject.devTalks.dto.response.ResponseDto;
+
+public interface InformationBoardService {
+
+    public ResponseEntity<ResponseDto> postInformationBoard(String userEmail, PostInformationBoardRequestDto dto);
+
+    public ResponseEntity<? super GetInformationBoardResponseDto> getBoard(Integer informationBoardNumber);
+    public ResponseEntity<? super GetInformationBoardListResponseDto> getBoardList();
+    
+    public ResponseEntity<ResponseDto> patchBoard(String userEmail, PatchInformationBoardRequestDto dto);
+    
+    public ResponseEntity<ResponseDto> deleteBoard(String userEmail, Integer boardNumber);
     
 }

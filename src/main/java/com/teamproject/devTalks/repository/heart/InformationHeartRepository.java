decfebc3cbb88f04703d448fefe31b653d523603
MINJU0401 <126844522+MINJU0401@users.noreply.github.com>
2023-05-14 00:17:22 +0900

    informationboardserviceimplement 수정중

--- src/main/java/com/teamproject/devTalks/repository/heart/InformationHeartRepository.java
+++ src/main/java/com/teamproject/devTalks/repository/heart/InformationHeartRepository.java
@@ -3,10 +3,12 @@ package com.teamproject.devTalks.repository.heart;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.teamproject.devTalks.entity.heart.InformationHeartEntity;
 import com.teamproject.devTalks.entity.primaryKey.information.InformationHeartPk;
 
+@Repository
 public interface InformationHeartRepository  extends JpaRepository<InformationHeartEntity, InformationHeartPk> {
 
     List<InformationHeartEntity> findAllByInformationBoardNumber(String informationBoardNumber);

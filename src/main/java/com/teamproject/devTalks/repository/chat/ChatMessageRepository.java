2841cb0df4d4341cd78fb9bb8c4baf5bb60a46d3
MINJU0401 <126844522+MINJU0401@users.noreply.github.com>
2023-05-22 22:22:47 +0900

    채팅관련 클래스 생성

---
+++ src/main/java/com/teamproject/devTalks/repository/chat/ChatMessageRepository.java
@@ -0,0 +1,7 @@
+package com.teamproject.devTalks.repository.chat;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatMessageRepository extends JpaRepository {
+    
+}
---
+++ src/main/java/com/teamproject/devTalks/repository/chat/ChatRoomRepository.java
@@ -0,0 +1,5 @@
+package com.teamproject.devTalks.repository.chat;
+
+public interface ChatRoomRepository {
+    
+}

a43f4fe0f9dac969811b73521f0855ee96f188a5
MINJU0401 <126844522+MINJU0401@users.noreply.github.com>
2023-05-22 22:26:53 +0900

    ChatServiceImplement 어노테이션 주입

--- src/main/java/com/teamproject/devTalks/repository/chat/ChatMessageRepository.java
+++ src/main/java/com/teamproject/devTalks/repository/chat/ChatMessageRepository.java
@@ -2,6 +2,6 @@ package com.teamproject.devTalks.repository.chat;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChatMessageRepository extends JpaRepository {
+public interface ChatMessageRepository {
     
 }
--- src/main/java/com/teamproject/devTalks/service/implement/chat/ChatServiceImplement.java
+++ src/main/java/com/teamproject/devTalks/service/implement/chat/ChatServiceImplement.java
@@ -3,11 +3,16 @@ package com.teamproject.devTalks.service.implement.chat;
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import com.teamproject.devTalks.dto.request.chat.CreateChatMessageRequestDto;
 import com.teamproject.devTalks.dto.response.ResponseDto;
 import com.teamproject.devTalks.service.chat.ChatService;
 
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class ChatServiceImplement implements ChatService {
 
 

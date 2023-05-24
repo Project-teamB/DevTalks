b1761b34a2c746a7e69d4d9e2807e3a31b034be5
JinDoL1998 <jkhgfdds1@gmail.com>
2023-05-24 09:48:33 +0900

    Information 검색 기능 추가

--- src/main/java/com/teamproject/devTalks/repository/board/RecruitBoardRepository.java
+++ src/main/java/com/teamproject/devTalks/repository/board/RecruitBoardRepository.java
@@ -124,7 +124,7 @@ public interface RecruitBoardRepository extends JpaRepository<RecruitBoardEntity
     "ON R.recruit_board_number = C.recruit_board_number " +
     "LEFT JOIN recruit_heart H " +
     "ON R.recruit_board_number = H.recruit_board_number " +
-    "WHERE recruit_board_title LIKE ? " +
+    "WHERE R.recruit_board_title LIKE ? " +
     "GROUP BY R.recruit_board_number " +
     "ORDER BY R.write_datetime DESC", nativeQuery = true
     )
@@ -147,7 +147,7 @@ public interface RecruitBoardRepository extends JpaRepository<RecruitBoardEntity
     "ON R.recruit_board_number = C.recruit_board_number " +
     "LEFT JOIN recruit_heart H " +
     "ON R.recruit_board_number = H.recruit_board_number " +
-    "WHERE recruit_board_title LIKE ? " +
+    "WHERE R.recruit_board_nickname LIKE ? " +
     "GROUP BY R.recruit_board_number " +
     "ORDER BY R.write_datetime DESC", nativeQuery = true
     )
